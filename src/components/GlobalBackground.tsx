98e5eb40c5cdd79697d4b76727f17a24cd5a9e58
Tyler Cannon <tylercannon@users.noreply.github.com>
2026-02-22 22:10:21 -0700

    Update stars component (#10)
    Co-authored-by: tylercannon <17668242+tylercannon@users.noreply.github.com>

--- src/components/GlobalBackground.tsx
+++ src/components/GlobalBackground.tsx
@@ -2,9 +2,8 @@ import Stars from "./Stars";
 
 const GlobalBackground = () => {
     return (
-        <div className="fixed inset-0 -z-10 pointer-events-none overflow-hidden">
-            <div className="absolute inset-0 bg-cosmic-depth" />
-            <Stars starColor="--color-neon-green" starCount={300} />
+        <div className="fixed inset-0 -z-10 pointer-events-none bg-cosmic-depth overflow-hidden">
+            <Stars starColor="--color-neon-green" starCount={400} />
         </div>
     );
 };
